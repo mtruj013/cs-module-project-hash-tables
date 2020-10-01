fd7ad6582d2fcea0863b327cc82ea5a0a13ebe1f
Maria Trujillo <mptruj015@gmail.com>
2020-10-01 17:08:11 +0200

    finished lookuptable and expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,18 @@
 # Your code here
+cache = {}
 
 
 def expensive_seq(x, y, z):
     # Your code here
 
+    if x <= 0:
+        return y + z
+
+    # check if inputs are in the cache
+    inputs = (x,y,z)
+    if inputs not in cache:
+        cache[inputs] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+    return cache[inputs]
 
 
 if __name__ == "__main__":
--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,10 +1,12 @@
 # Your code here
-
+import math
+import random
+cache = {}
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
     v = math.factorial(v)
-    v //= (x + y)
+    v //= (x + y) # divides and rounds down
     v %= 982451653
 
     return v
@@ -15,6 +17,18 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+
+    # set target
+    target = str(f'{x}, {y}')
+
+    # check if its in the cach
+    if target not in cache:
+        # if it's not, add it 
+        cache[target] = slowfun_too_slow(x, y)
+        return cache[target]
+    else:
+        return cache[target]
+
 
 
 
